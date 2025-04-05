3cdc9b5ef0ba791afb75ab25b8b6df33fc0c55e0
Davit-Glunchadze <davitiglunchadze@gmail.com>
2025-04-05 12:50:19 +0400

    Add condition of the task

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+
+// მოახდინე პროექტის ინიციალიზაცია
+// ● დააინსტალირე პაკეტი validator
+// ● მოიძიე validator-ის დოკუმენტაცია
+// npmjs.com - ზე
+// ● დოკუმენტაციის მიხედვით გამოიყენე
+// ბრძანება isEmail და დაბეჭდე არის თუ არა
+// შემდეგი სიტყვები მეილი:
+// ● test@test.com, abcDE123
+
+
 var validator = require('validator');
 
 const TrueEmail = validator.isEmail('test@test.com')
